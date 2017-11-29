b6126662494412b3684175af2054f061ef39b173
twn39 <twn39@163.com>
2017-11-29 18:12:29 +0800

    update

--- src/handlers/IndexHandler.js
+++ src/handlers/IndexHandler.js
@@ -1,10 +1,10 @@
 const IndexHandler = {
     index(ctx, next) {
         ctx.logger.info('logger....');
-        ctx.body = JSON.stringify({
+        ctx.body = {
             title: 'Welcome to this site.',
             version: '1.0.1',
-        });
+        };
     },
 };
 
