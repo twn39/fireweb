fb9d9a5eb493a54caf435f4c63eb3f8c8ffa1583
twn39 <twn39@163.com>
2017-11-29 14:56:41 +0800

    update

--- src/models/DB.js
+++ src/models/DB.js
@@ -1,4 +1,5 @@
 const config = {
+  /*
   client: 'mysql',
   connection: {
     host: process.env.DB_HOST,
@@ -7,7 +8,12 @@ const config = {
     database: process.env.DB_NAME,
     charset: process.env.DB_CHARSET,
   },
+  */
+    client: 'pg',
+    connection: process.env.PG_CONNECTION,
+    searchPath: ['knex', 'public'],
 };
+
 const knex = require('knex')(config);
 
 const bookshelf = require('bookshelf')(knex);
