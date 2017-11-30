7756a78485b82034176cd7b80e262edce9b62b7b
twn39 <twn39@163.com>
2017-11-30 18:31:39 +0800

    update

--- src/models/DB.js
+++ src/models/DB.js
@@ -12,6 +12,7 @@ const config = {
     client: 'pg',
     connection: process.env.PG_CONNECTION,
     searchPath: ['knex', 'public'],
+    pool: { min: 0, max: 80 }
 };
 
 const knex = require('knex')(config);
