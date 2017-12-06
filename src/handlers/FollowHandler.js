fc2369edc8af151ffce93a95c2e3341b600b17c6
twn39 <twn39@163.com>
2017-12-06 11:13:31 +0800

    update

--- src/handlers/FollowHandler.js
+++ src/handlers/FollowHandler.js
@@ -1,7 +1,48 @@
-const FollowHandler = {
-    async follow(ctx, next) {},
+const {
+    SUCCESS,
+    SERVER_ERROR
+} = require('../helpers/ErrorCode');
+const Code = require('../helpers/Code');
+const FollowRepo = require('../repositories/FollowRepository');
 
-    async unFollow(ctx, next) {},
+const FollowHandler = {
+    /**
+     * router: GET /v1/users/{id}/follow
+     * @param ctx
+     * @param next
+     * @returns {Promise<*>}
+     */
+    async follow(ctx, next) {
+        const uid = ctx.state.jwt.uid;
+        const followUserId = ctx.params.id;
+
+        const success = await FollowRepo.follow(uid, followUserId);
+
+        if (success) {
+            return ctx.body = Code(SUCCESS);
+        }
+
+        return ctx.body = Code(SERVER_ERROR);
+    },
+
+    /**
+     * router: GET /v1/users/{id}/unfollow
+     * @param ctx
+     * @param next
+     * @returns {Promise<*>}
+     */
+    async unFollow(ctx, next) {
+        const uid = ctx.state.jwt.uid;
+        const followUserId = ctx.params.id;
+
+        const success = await FollowRepo.unFollow(uid, followUserId);
+
+        if (success) {
+            return ctx.body = Code(SUCCESS);
+        }
+
+        return ctx.body = Code(SERVER_ERROR);
+    },
 };
 
 module.exports = FollowHandler;
